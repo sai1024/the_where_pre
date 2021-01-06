32c6bbe08b97d7c85418f2319615b920e0bd6d45
sai <rubyer1993@gmail.com>
2021-01-06 11:52:16 +0800

    init

---
+++ lib/the_where_pre.rb
@@ -0,0 +1,6 @@
+require "the_where_pre/version"
+
+module TheWherePre
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/the_where_pre/version.rb
@@ -0,0 +1,3 @@
+module TheWherePre
+  VERSION = "0.1.0"
+end
